670c9f8813ef55cdf675209ca5046ccee28ba9a8
Igor M <igor_mech@hotmail.com>
2017-06-12 14:19:16 -0400

    Start Main loop

--- LogParser/index.js
+++ LogParser/index.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const fs = require("fs");
+
+const log = fs.readFileSync("./log.txt", "UTF8");
+
+var logSplit = log.split("\n");
+
+console.log(logSplit);
+
+
+var apiConnect = false;
+var wsAdapter = false;
+
+
+for(var i=0;i<logSplit.length();i++){
+
+	if(!apiConnect)
+
+}
